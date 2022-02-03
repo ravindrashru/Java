01500549aa638967f5d2e956cc8eb0caec7112c5
ravindra <ravindrakulkarni99@gmail.com>
2022-02-03 07:01:15 +0530

    Done

---
+++ Constructor/src/DemoConstructor.java
@@ -0,0 +1,18 @@
+
+public class DemoConstructor {
+	
+	int a;
+	
+	public DemoConstructor() {
+		
+		System.out.println("Default constructor is called");
+	}
+	
+	public DemoConstructor(int a) {
+		this.a=a;
+		System.out.println("Parameterised constructor is called");
+		
+	}
+
+}
+
---
+++ Constructor/src/FinalConstructor.java
@@ -0,0 +1,12 @@
+
+public class FinalConstructor {
+
+	public static void main(String[] args) {
+		DemoConstructor obj1=new DemoConstructor();
+		DemoConstructor obj2=new DemoConstructor(20);
+		
+		
+
+	}
+
+}
