b4ad2cd375cd6f52ff3ba0bc360b7c239f8077c4
ravindra <ravindrakulkarni99@gmail.com>
2022-02-21 11:37:03 +0530

    Done

---
+++ ValidateUserLogin/src/main/java/Myservlet.java
@@ -0,0 +1,51 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Myservlet
+ */
+public class Myservlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Myservlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		 
+	    PrintWriter out = response.getWriter();  
+	          
+	    String n=request.getParameter("username");  
+	    String p=request.getParameter("password");
+	    
+	    if(n.equalsIgnoreCase("admin") && p.equals("Admin@123")) {
+	    	out.write("UserId and Password is valid");
+	     }
+	    else {
+	    	out.write("UserId and Password is invalid");
+	    }
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
